7f6277e360a7fda1e3311c41bce952d7b732275a
jn-alvarez <nahuelvera212@gmail.com>
2026-06-29 18:09:07 -0300

    Create App.jsx

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Joel Nahuel Alvarez Vera</h1>
+      <h2>Software Developer</h2>
+      <h2>AI & Automation Specialist</h2>
+    </>
+  );
+}
+
+export default App;
